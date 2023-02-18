f54146f2d64c527673b39931062f96d63e0c5351
Carlos Del Castillo M <maussac13@gmail.com>
2023-02-18 15:07:51 -0500

    Se agrega el modulo FormsModule para usar [(ngModel)]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OwnerComponent } from './owner/owner.component';
 import { PetComponent } from './pet/pet.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     OwnerComponent,
-    PetComponent
+    PetComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
